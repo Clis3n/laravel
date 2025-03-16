ec36a0289fb90cb0adf4f57f0fc459c50cd0e099
Clis3n <clisenardylaksonowicaksono@mail.ugm.ac.id>
2025-03-16 15:14:53 +0700

    Add files via upload

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -84,6 +84,42 @@
             </div>
         </div>
     </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="CreatePolygonModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Crate Polygon</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form method="POST" action="{{ route('polygon.store') }}">
+                <div class="modal-body">
+                        @csrf
+
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="name" name="name"
+                                placeholder="Polygon Name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="description" class="form-label">Description</label>
+                            <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="geom_polygon" class="form-label">Geometry</label>
+                            <textarea class="form-control" id="geom_polygon" name="geom_polygon" rows="3"></textarea>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
 @endsection
 
 @section('script')
@@ -139,6 +175,8 @@
                 $('#CreatePolylineModal').modal('show');
             } else if (type === 'polygon' || type === 'rectangle') {
                 console.log("Create " + type);
+                $('#geom_polygon').val(objectGeometry);
+                $('#CreatePolygonModal').modal('show');
             } else if (type === 'marker') {
                 console.log("Create " + type);
                 $('#geom_point').val(objectGeometry);

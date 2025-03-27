4f28f555263959de19efee6685044167263538d1
Clis3n <clisenardylaksonowicaksono@mail.ugm.ac.id>
2025-03-27 12:41:39 +0700

    Add files via upload
    update

--- app/Models/PolylinesModel.php
+++ app/Models/PolylinesModel.php
@@ -2,10 +2,37 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 
 class PolylinesModel extends Model
 {
     protected $table = 'polylines';
     protected $guarded = ['id'];
+
+    public function geojson_polylines()
+    {
+        $polylines = $this->select(DB::raw("id, ST_AsGeoJSON(geom) AS geom, ST_Length(geom, true) AS panjang_m, ST_Length(geom, true)/1000 AS panjang_km, name, description, created_at, updated_at"))->get();
+        $geojson = [
+            'type' => 'FeatureCollection',
+            'features' => []
+        ];
+
+        foreach ($polylines as $p) {
+            $feature = [
+                'type' => 'Feature',
+                'geometry' => json_decode($p->geom),
+                'properties' => [
+                    'name' => $p->name,
+                    'description' => $p->description,
+                    'created_at' => $p->created_at,
+                    'updated_at' => $p->updated_at,
+                    'panjang_m' => $p->panjang_m,
+                    'panjang_km' => $p->panjang_km,
+                ],
+            ];
+            array_push($geojson['features'], $feature);
+        }
+        return $geojson;
+    }
 }

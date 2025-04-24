53fc8f377d797a8316a8390f7042e91f0a69a012
clis3n <clisenardylaksonowicaksono@mail.ugm.ac.id>
2025-04-24 18:53:04 +0700

    acara 7

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,41 +1,30 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark"> {{-- Ganti bg-body-tertiary menjadi bg-dark dan tambahkan data-bs-theme="dark" --}}
     <div class="container-fluid">
-        <a class="navbar-brand" href="#"><i class="fa-solid fa-earth-americas"></i>{{ $title }}</a>
+        <a class="navbar-brand" href="{{ url('/') }}"> {{-- Arahkan ke home/root URL --}}
+            <i class="fa-solid fa-earth-americas"></i> {{ $title }}
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    {{-- Tentukan link aktif berdasarkan halaman saat ini jika perlu --}}
+                    {{-- Contoh sederhana: jika route saat ini adalah 'home' (jika ada), tambahkan 'active' --}}
+                    <a class="nav-link {{ request()->is('/') ? 'active' : '' }}" aria-current="page" href="{{ url('/') }}">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('map') }}">Map</a>
+                     {{-- Contoh sederhana: jika route saat ini adalah 'map', tambahkan 'active' --}}
+                    <a class="nav-link {{ Route::currentRouteName() == 'map' ? 'active' : '' }}" href="{{ route('map') }}">Map</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('table') }}">Table</a>
+                     {{-- Contoh sederhana: jika route saat ini adalah 'table', tambahkan 'active' --}}
+                    <a class="nav-link {{ Route::currentRouteName() == 'table' ? 'active' : '' }}" href="{{ route('table') }}">Table</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                </li>
+                {{-- HAPUS Dropdown --}}
+                {{-- HAPUS Link Disabled --}}
             </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            {{-- HAPUS Form Search --}}
         </div>
     </div>
 </nav>
